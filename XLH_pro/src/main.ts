07759ad8577898ecd3a9499945475ce5ee0effef
xilanhuahua <111215262+xilanhuahua@users.noreply.github.com>
2024-05-27 20:10:51 +0800

    add
    add

--- XLH_pro/src/main.ts
+++ XLH_pro/src/main.ts
@@ -1,13 +1,6 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-05-27 18:18:44
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-05-27 18:30:18
- * @FilePath: \xlhproje\XLH_pro\src\main.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+createApp(App).use(store).use(router).mount("#app");
--- XLH_pro/src/router/index.ts
+++ XLH_pro/src/router/index.ts
@@ -1,33 +1,26 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-05-27 18:21:25
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-05-27 18:28:40
- * @FilePath: \xlhproje\XLH_pro\src\router\index.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-const routes: RouteRecordRaw[] = [
+const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/main'
-  }
-  // ,
-  // {
-  //   path: '/main',
-  //   component: () => import('../views/main/main.vue')
-  // },
-  // {
-  //   path: '/login',
-  //   component: () => import('../views/login/login.vue')
-  // }
-]
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
+  history: createWebHashHistory(),
   routes,
-  history: createWebHashHistory()
-})
+});
 
-export default router
+export default router;
